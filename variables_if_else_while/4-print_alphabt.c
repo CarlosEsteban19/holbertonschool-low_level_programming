ebe58299f0931410925c687d4d9d934045cabcc3
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-20 15:05:50 -0400

    Printing alphabet excluding e & q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int alphabet = 97;
+
+	while (alphabet <= 122)
+{
+	if (alphabet != 101 && alphabet != 113)
+		putchar(alphabet);
+		alphabet++;
+}
+	putchar('\n');
+	return (0);
+}
+
