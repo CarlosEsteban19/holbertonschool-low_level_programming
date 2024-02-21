f473058468730fa47f35fb53d89be1e3e9a56f1b
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-20 22:35:06 -0400

    Printing 0 to 9 with while loop, ascii values and putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
