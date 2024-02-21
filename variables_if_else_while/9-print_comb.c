d7929e69ad28441213c57bc9e82b5ece26815b6a
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-21 13:03:12 -0400

    Printing numbers with comma and space in between

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		if (num != 57)
+		putchar(',');
+		putchar(' ');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
