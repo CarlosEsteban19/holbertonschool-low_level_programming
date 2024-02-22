9bb25bb44ceb56140af48736359df473f912fd6a
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-22 11:11:57 -0400

    Printing 10 alphabets

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: (void)
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet;
+	int repeat = 0;
+
+	while (repeat < 10)
+	{
+		alphabet = 'a';
+		while(alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		_putchar('\n');
+		repeat++;
+	}
+}
