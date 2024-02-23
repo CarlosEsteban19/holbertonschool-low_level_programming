f5b083b8c1c6f66d4d66326671b01848a8079f7c
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-23 12:11:09 -0400

    printing every minute in 24 hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: (void)
+ */
+void jack_bauer(void)
+{
+	char houra = '0';
+	char hourb = '0';
+	char mina = '0';
+	char minb = '0';
+
+	while (minb <= '9')
+	{
+		if (houra == '2' && hourb == '4')
+		break;
+		_putchar(houra);
+		_putchar(hourb);
+		_putchar(':');
+		_putchar(mina);
+		_putchar(minb);
+		_putchar('\n');
+		minb++;
+		if (minb == ':')
+		{
+			minb = '0';
+			mina++;
+			if (mina == '6')
+			{
+				minb = '0';
+				mina = '0';
+				hourb++;
+				if (hourb == ':')
+				{
+					minb = '0';
+					mina = '0';
+					hourb = '0';
+					houra++;
+				}
+			}
+		}
+	}
+}
