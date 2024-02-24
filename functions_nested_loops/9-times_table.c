35168e523194edda23fd607189b5b01bda216c2f
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-23 21:36:09 -0400

    Printing multiplication tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 			result = num * multiplo;
 			if (result <= 9)
 				_putchar(' ');
-			else 
+			else
 				_putchar((result / 10) + '0');
 			_putchar((result % 10) + '0');
 		}
