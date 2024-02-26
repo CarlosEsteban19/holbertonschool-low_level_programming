489294a7f74e90348de039fb1cc39aad077a0fce
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-26 11:14:18 -0400

    printing numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers and returns result.
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: Result of a + b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: (void)
+ */
+void more_numbers(void)
+{
+	int num, rep;
+
+	for (rep = 0; rep < 11; rep++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
