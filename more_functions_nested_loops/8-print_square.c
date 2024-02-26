910ee2119bdc789a93ab7b2e262f46afa835d565
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-26 14:23:53 -0400

    prints a square with hashtags

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *@size: size of square
+ * Return: (int)
+ */
+void print_square(int size)
+{
+	int lenght = 0;
+	int width;
+
+	while (lenght < size)
+	{
+		width = size;
+		while (width > 0)
+		{
+			_putchar(35);
+			width--;
+		}
+		_putchar('\n');
+		lenght++;
+	}
+}
