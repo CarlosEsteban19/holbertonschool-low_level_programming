b4b77a4ee958da60592fc5316f0881273518706e
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-26 13:27:29 -0400

    Prints diagonal wit given lenght

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: lenght of diagonal
+ * Return: (int)
+ */
+void print_diagonal(int n)
+{
+	int space;
+	int line = 0;
+
+	if (n > 0)
+	{
+		for (; n != 0; n--)
+		{
+			_putchar(92);
+			_putchar('\n');
+			line++;
+			space = line;
+			for (; space > 0; space--)
+				_putchar(' ');
+		}
+	}
+	else
+		_putchar('\n');
+}
