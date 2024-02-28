a9a9ce79680d802f9d3603bec28df8afbeef4270
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-28 09:36:29 -0400

    Printing given string skiping every other character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: given string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	char *pointer = str;
+
+	while (*pointer)
+	{
+		_putchar(*pointer);
+		pointer+=2;
+	}
+	_putchar('\n');
+}
