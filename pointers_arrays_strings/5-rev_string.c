3f1a61c0bb23474d6be8f2f51c0ad8d7def1f924
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-28 00:03:06 -0400

    Funtion to reverse a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int size = 0;
+	int limit;
+	char ditto;
+	char *pointer = s;
+
+	while (s[size] != '\0')
+		size++;
+
+	limit = size / 2;
+
+	while (*pointer != s[limit])
+	{
+		ditto = s[size - 1];
+		s[size - 1] = *pointer;
+		*pointer = ditto;
+		size--;
+		pointer++;
+	}
+}
