31aaa6bdc94f03556d9949b11cf153ca6e143f52
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-02-29 15:43:31 -0400

    Compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings.
  * @s1: 1st string
  * @s2: 2nd string
- * return: difference between 1st pair of chars that dont match.
+ * Return: difference between 1st pair of chars that dont match.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -14,7 +14,8 @@ int _strcmp(char *s1, char *s2)
 	diff = s1[0] - s2[0];
 	while (diff == 0)
 	{
-		if (s1[i] == '\0') break;
+		if (s1[i] == '\0')
+			break;
 		i++;
 		diff = s1[i] - s2[i];
 	}
