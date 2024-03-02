d6885b80eb537363b18753dc87b64b137a615147
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-01 23:34:00 -0400

    print numbers

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,43 +7,33 @@
  */
 void print_number(int n)
 {
-	if (n > 0)
-	{
-		if (n >= 1000)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar(((n % 1000) / 100) + '0');
-			_putchar((((n % 1000) % 100) / 10) + '0');
-		}
-		else if (n >= 100)
-		{
-			_putchar((n / 100) + '0');
-			_putchar(((n % 100) / 10) + '0');
-		}
-		else if (n >= 10)
-			_putchar((n / 10) + '0');
+	int divide = 1;
+	int zeros = 1;
+	int num;
 
-		_putchar((n % 10) + '0');
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
 	}
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n <= -1000)
-		{
-			_putchar(((n * -1) / 1000) + '0');
-			_putchar((((n * -1) % 1000) / 100) + '0');
-			_putchar(((((n * -1) % 1000) % 100) / 10) + '0');
-		}
-		else if (n <= -100)
-		{
-			_putchar(((n * -1) / 100) + '0');
-			_putchar((((n * -1) % 100) / 10) + '0');
-		}
-		else if (n <= -10)
-			_putchar(((n * -1) / 10) + '0');
-
-		_putchar(((n * -1) % 10) + '0');
+		n = -n;
 	}
-	if (n == 0)
-		_putchar('0');
+	while (n / divide > 9)
+	{
+		divide *= 10;
+	}
+	while (divide >= 1)
+	{
+		num = (n / divide) % 10;
+
+		if (digit != 0 || !zeros)
+		{
+			_putchar(num + '0');
+			zeros = 0;
+		}
+		divisor /= 10;
+	}
 }
