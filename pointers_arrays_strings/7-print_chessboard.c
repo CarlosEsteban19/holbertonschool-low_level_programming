6b90606ce831fbb1af2adb64f0409bf71403eac5
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-04 22:07:28 -0400

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int line = 0;
+	int size;
+	char *pointer;
+
+	for (pointer = a[line]; line < 8; line++)
+	{
+		for (size = 0; size < 8; size++, pointer++)
+		{
+			_putchar(*pointer);
+		}
+		_putchar('\n');
+	}
+}
