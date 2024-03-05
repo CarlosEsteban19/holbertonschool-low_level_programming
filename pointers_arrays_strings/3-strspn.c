519225c4c1235dad15ab9d2e7c146d0205867db9
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-04 20:14:50 -0400

    gets length of string

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,11 +9,14 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int size;
-	unsigned int count;
+	unsigned int count = 0;
 	unsigned int length = 0;
 
 	for (size = 0; s[size] != '\0'; size++)
 	{
+		if (accept[count] == '\0')
+			return (length);
+
 		for (count = 0; accept[count] != '\0'; count++)
 		{
 			if (s[size] == accept[count])
@@ -23,5 +26,5 @@ unsigned int _strspn(char *s, char *accept)
 			}
 		}
 	}
-	return (length - 2);
+	return (length);
 }
