db3fad57db186696991b0c2a19aa698006392231
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-06 10:02:28 -0400

    printing with recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
-		if (*s == '\0')
-			_putchar('\n');
-		_puts_recursion(s);
+		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
