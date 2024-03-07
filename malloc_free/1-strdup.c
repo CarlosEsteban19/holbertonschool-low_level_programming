8b75d452ad394d8f9d93d9361f14db65bcd2902d
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-07 16:17:23 -0400

    copies string into new memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - copies string into new memory
+ * @str: string
+ * Return: ponter to copied string or NULL
+ */
+char *_strdup(char *str)
+{
+	char *pointer;
+	unsigned int size;
+	int index;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+	{}
+
+	pointer = malloc(size * sizeof(char));
+	if (pointer == NULL)
+		return (NULL);
+
+	for (index = 0; pointer[index] == '\0'; index++)
+		pointer[index] = str[index];
+
+	return (pointer);
+}
