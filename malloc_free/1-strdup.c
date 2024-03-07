aa6366fb7ec5baab454c47cff55ed7ca8f655b70
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-07 16:19:20 -0400

    copies string into new memory

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	for (size = 0; str[size] != '\0'; size++)
 	{}
 
-	pointer = malloc(size * sizeof(char));
+	pointer = malloc((size - 1) * sizeof(char));
 	if (pointer == NULL)
 		return (NULL);
 
