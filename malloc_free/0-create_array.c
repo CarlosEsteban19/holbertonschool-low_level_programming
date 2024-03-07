8ca54195e2bb5f243dcd05bfdf870c89345a24c9
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-07 15:44:57 -0400

    creating string initialized

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - creates an array of chars
+ * @size: size
+ * @c: character
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *pointer;
+	char *otro;
+
+	if (size == 0)
+		return (NULL);
+
+	pointer = malloc(size + 1 * sizeof(char));
+	otro = pointer;
+
+	if (pointer == NULL)
+		return (NULL);
+
+	while (*otro == '\0')
+	{
+		*otro = c;
+		otro++;
+	}
+	return(pointer);
+}
