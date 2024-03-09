6ed37c226dc42de6c965c8cb331c4b0ed6b7a220
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-09 16:35:33 -0400

    concatenate 2 strings up to certain amount of bytes

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 	{}
 
-	pointer = malloc((size1 + size2) * sizeof(char));
+	pointer = malloc((size1 + size2 + 1) * sizeof(char));
 	if (pointer == NULL)
 		return (NULL);
 
