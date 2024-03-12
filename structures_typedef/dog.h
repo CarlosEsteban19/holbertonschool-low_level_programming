7dee771f259402ba31fd80e30cfa7e10b36b8776
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-12 10:44:01 -0400

    dog struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure with doggies
+ * @name: name of doggies
+ * @owner: owner of doggies
+ * @age: how old be doggies
+ */
+struct dog
+{
+char *name;
+char *owner;
+float age;
+};
+#endif /* DOG_H */
