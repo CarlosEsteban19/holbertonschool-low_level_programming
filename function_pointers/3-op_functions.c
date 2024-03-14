f509c25f27eee9b6e8e7f5a678bd4ab12597099e
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-14 12:30:47 -0400

    operator functions for calculator

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,6 +3,7 @@
  * op_add - returns sum of 2 integers
  * @a: 1st integer
  * @b: 2nd integer
+ * Return: sum
  */
 int op_add(int a, int b)
 {
@@ -12,6 +13,7 @@ int op_add(int a, int b)
  * op_sub - returns difference of 2 integers
  * @a: 1st integer
  * @b: 2nd integer
+ * Return: difference
  */
 int op_sub(int a, int b)
 {
@@ -21,6 +23,7 @@ int op_sub(int a, int b)
  * op_mul - returns product of 2 integers
  * @a: 1st integer
  * @b: 2nd integer
+ * Return: product
  */
 int op_mul(int a, int b)
 {
@@ -30,6 +33,7 @@ int op_mul(int a, int b)
  * op_div - returns division of 2 integers
  * @a: 1st integer
  * @b: 2nd integer
+ * Return: division
  */
 int op_div(int a, int b)
 {
@@ -39,6 +43,7 @@ int op_div(int a, int b)
  * op_mod - returns remainder of 2 integers
  * @a: 1st integer
  * @b: 2nd integer
+ * Return: remainder
  */
 int op_mod(int a, int b)
 {
