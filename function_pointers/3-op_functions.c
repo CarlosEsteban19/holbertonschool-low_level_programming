e17a8665a143c2a10556b8aecce606c80af6b217
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-03-14 12:14:35 -0400

    operator functions for calculator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+/**
+ * op_add - returns sum of 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns difference of 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns product of 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns division of 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns remainder of 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
