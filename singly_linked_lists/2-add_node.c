83bea590e16c6f922a4dcfd8cf27b80e68eee270
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-03 11:15:48 -0400

    adding node at beginning

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strlen - Calculates the length of the string
+ * @s: The stirng being measured
+ * Return: int (Success)
+ */
+int _strlen(const char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{}
+	return (l);
+}
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head:
+ * @str:
+ * Return: list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof (list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
