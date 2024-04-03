2068e2f38b8d35b627c7af453f284bfaa81bc477
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-03 14:14:07 -0400

    returning a specific node

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a doubly list
+ * @head: beginning of list
+ * @index: node to return
+ * Return: node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int num = 0;
+
+	while (num < index)
+	{
+		head = head->next;
+		num++;
+	}
+	return (head);
+}
