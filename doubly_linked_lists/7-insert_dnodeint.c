ea8102d83427128513edec05e98670e50bad279d
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-03 15:14:33 -0400

    inserting node at given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,6 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			limit++;
 			ditto = ditto->next;
 		}
+		if (idx > limit)
+			return (NULL);
 		new->prev = ditto;
 		new->n = n;
 		new->next = ditto->next;
