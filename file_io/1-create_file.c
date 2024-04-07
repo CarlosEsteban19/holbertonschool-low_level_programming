9c1d4aff400832718747d358ccd387f8f852805d
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-07 15:21:49 -0400

    creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	char *ptr = text_content;
-	ssize_t bytesWR, len;
+	ssize_t bytesWR, len = 0;
 
 	if (filename == NULL)
 		return (-1);
