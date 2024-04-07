e11867a2f35ec03e058ed262bb028384446530c6
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-07 19:51:45 -0400

    print error helper

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
  */
 int print_error(const char *msg, int code, int fd_from, int fd_to)
 {
-	dprintf(STDERR_FILENO, "Error: %s\n", msg);
+	dprintf(STDERR_FILENO, "%s\n", msg);
 	if (fd_from != -1)
 		close(fd_from);
 	if (fd_to != -1)
@@ -34,25 +34,25 @@ int main(int argc, char *argv[])
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		return (print_error("Can't read from file", 98, fd_from, fd_to));
+		return (print_error("Error: Can't read from file", 98, fd_from, fd_to));
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
-		return (print_error("Can't write to file", 99, fd_from, fd_to));
+		return (print_error("Error: Can't write to file", 99, fd_from, fd_to));
 
 	while ((bytesRD = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytesWR = write(fd_to, buffer, bytesRD);
 		if (bytesWR != bytesRD)
-			return (print_error("Can't write to file", 99, fd_from, fd_to));
+			return (print_error("Error: Can't write to file", 99, fd_from, fd_to));
 	}
 
 	if (bytesRD == -1)
-		return (print_error("Can't read from file", 98, fd_from, fd_to));
+		return (print_error("Error: Can't read from file", 98, fd_from, fd_to));
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
-		return (print_error("Can't close fd", 100, fd_from, fd_to));
+		return (print_error("Error: Can't close fd ", 100, fd_from, fd_to));
 
 	return (0);
 }
