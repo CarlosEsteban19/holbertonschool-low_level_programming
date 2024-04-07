1a27c26f81148d9fc2623fe36723b39c4b46ef43
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-07 15:18:45 -0400

    creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,6 +8,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	char *ptr = text_content;
 	ssize_t bytesWR, len;
 
 	if (filename == NULL)
@@ -17,15 +18,17 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-		while (text_content[len] != '\0')
-			len++;
+	while (ptr != '\0')
+		len++, ptr++;
 
-	bytesWR = write(fd, text_content, len);
-	if (bytesWR == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		bytesWR = write(fd, text_content, len);
+		if (bytesWR == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
