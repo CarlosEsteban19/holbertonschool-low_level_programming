21e6d75e2b47279112e750976a1b9d61673209c8
CarlosEsteban19 <carlos.esteban19@outlook.com>
2024-04-07 16:57:42 -0400

    copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,51 +1,65 @@
 #include "main.h"
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
 /**
  * main - Entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: int
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 97-100 on errors
  */
 int main(int argc, char *argv[])
 {
-	int result;
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-	ssize_t bytesRD;
+	int fd_from, fd_to;
 	char buffer[1024];
+	ssize_t bytesRD, bytesWR;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	bytesRD = read_textfile(file_from, sizeof(buffer));
-	if (bytesRD == -1)
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		return (98);
 	}
-	result = create_file(file_to, buffer);
-	if (result == -1)
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		close(fd_from);
 		return (99);
 	}
-	if (close(STDIN_FILENO) == -1)
+
+	while ((bytesRD = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", STDIN_FILENO);
+		bytesWR = write(fd_to, buffer, bytesRD);
+		if (bytesWR != bytesRD)
+		{
+			dprintf(STDERR_FILENO, "Error: Write to file %s failed\n", file_to);
+			close(fd_from);
+			close(fd_to);
+			return (99);
+		}
+	}
+
+	if (bytesRD == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Read from file %s failed\n", file_from);
+		close(fd_from);
+		close(fd_to);
+		return (98);
+	}
+
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptors\n");
 		return (100);
 	}
-	if (close(STDOUT_FILENO) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", STDOUT_FILENO);
-		return (100);
-	}
-	if (close(STDERR_FILENO) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", STDERR_FILENO);
-		return (100);
-	}
-	return (0);
+
+	return 0;
 }
+
